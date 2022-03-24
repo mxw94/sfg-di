3b367417f25f2085a043c2bfece708fc533fe202
mxw94 <maximilian@wessel-bs.de>
2022-03-24 15:26:58 +0100

    di-assignment solution

--- src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
@@ -1,12 +1,13 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/27/19.
  */
-@Profile({"ES", "default"})
+@Profile({"ES", "default", "dog", "cat"})
 @Service("i18nService")
 public class I18NSpanishService implements GreetingService {
     @Override
