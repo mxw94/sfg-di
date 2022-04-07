2d4eadff6fb2727681ad3e72bd51454f2c19bf62
mxw94 <maximilian@wessel-bs.de>
2022-04-07 14:36:04 +0200

    after section 5

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,66 @@
+package guru.springframework.sfgdi.config;
+
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
+import guru.springframework.sfgdi.services.*;
+import org.springframework.context.annotation.*;
+
+@ImportResource("classpath:sfgdi-config.xml")
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    PetServiceFactory petServiceFactory() {
+        return new PetServiceFactory();
+    }
+
+    @Bean
+    @Profile({"dog", "default"})
+    PetService dogPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("dog");
+    }
+
+    @Bean
+    @Profile({"cat", "default"})
+    PetService catPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("cat");
+    }
+
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository() {
+        return new EnglishGreetingRepositoryImpl();
+    }
+
+    @Bean
+    @Primary
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
+
+    @Bean
+    @Profile("EN")
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository) {
+        return new I18nEnglishGreetingService(englishGreetingRepository);
+    }
+
+    @Bean("i18nService")
+    @Profile({"ES", "default"})
+    I18NSpanishService i18NSpanishService() {
+        return new I18NSpanishService();
+    }
+
+//    @Bean
+//    ConstructorGreetingService constructorGreetingService() {
+//        return new ConstructorGreetingService();
+//    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService() {
+        return new SetterInjectedGreetingService();
+    }
+}
